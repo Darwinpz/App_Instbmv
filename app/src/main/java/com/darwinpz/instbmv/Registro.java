2800420eeecad42f63b56d3d98f36c103a2db7cb
Darwinpz <dpilaloazea@gmail.com>
2023-12-04 21:16:11 -0500

    Conexión con Firebase App

--- app/src/main/java/com/darwinpz/instbmv/Registro.java
+++ app/src/main/java/com/darwinpz/instbmv/Registro.java
@@ -1,14 +1,24 @@
 package com.darwinpz.instbmv;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class Registro extends AppCompatActivity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar.setOnClickListener(view -> {
+            finish();
+        });
+
+
     }
 }
